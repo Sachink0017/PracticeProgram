95287dfd65dbe6de166a07d098b9e56bfd13c915
Sachink0017 <johndoe@example.com>
2025-09-14 22:36:14 +0530

    IMPLEMTED MERGE SORT

--- sorting.cpp
+++ sorting.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//SELECTION SORT 
 void selectionSort(vector<int> &arr)
 {
     for (int i = 0; i < 6; i++)
@@ -21,6 +22,55 @@ void selectionSort(vector<int> &arr)
     }
 }
 
+//MERGE SORT
+void merge(vector<int> &arr,int low,int high,int mid)
+{
+    
+    vector<int> temp;
+    int left=low;
+    int right=mid+1;
+    while (left  <= mid && right<= high)
+    {
+        if(arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++)
+    {
+        arr[i]=temp[i-low];
+    }
+
+}
+void mergeSort(vector<int> &arr, int low,int high)
+{
+    if(low >= high) 
+    {
+        return;
+    }
+
+    int mid=low+(high-low)/2;
+    mergeSort(arr,low,mid);
+    mergeSort(arr,mid+1,high);
+    merge(arr,low,high,mid);
+}
+
+
 int main()
 {
     int n;
@@ -34,8 +84,11 @@ int main()
     }
     cout << "this is the sorting algorithm:" << endl;
     cout << "selection sort:" << endl;
-    selectionSort(arr);
-    for (int i = 0; i < 6; i++)
+    // selectionSort(arr);
+
+    mergeSort(arr,0,n-1);
+    
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << endl;
     }
